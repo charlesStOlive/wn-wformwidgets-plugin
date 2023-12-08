2b81978bf8bbf99b8a7907c724a7e37e4cb3815f
Charles <charles@notilac.fr>
2023-12-08 12:05:19 +0100

    update global apres majedm

--- formwidgets/recordsfinder/partials/_container.php
+++ formwidgets/recordsfinder/partials/_container.php
@@ -1,4 +1,4 @@
-<?php if ($this->previewMode && !isset($value)): ?>
+<?php if ($this->previewMode && !isset($selectedValues)): ?>
 
     <span class="form-control" disabled="disabled"><?= e(trans('waka.wformwidgets::lang.form.preview_no_record_message')) ?></span>
 
